4b0aea0a8c3b6a691ce2dcabad55282302583b31
Calyx-Phil <philipocansey722@gmail.com>
2023-03-24 06:01:49 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,12 +21,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(nums, int));
 
 		if (index != (n - 1) && separator != NULL)
-		{
 			printf("%s", separator);
-		}
-
-		printf("\n");
-
-		va_end(nums);
 	}
+
+	printf("\n");
+
+	va_end(nums);
+
 }
